ef9980eb114afeab2f3b160325bccb9a31da6e87
shumik-vladyslav <shumik.vladyslav@gmail.com>
2016-09-29 13:05:31 +0300

    f

--- src/app/services/tempDialog.service.js
+++ src/app/services/tempDialog.service.js
@@ -6,7 +6,7 @@
         .factory('TempDialogService', TempDialogService);
 
     /** @ngInject */
-    function TempDialogService($http, $mdDialog) {
+    function TempDialogService($http, $mdDialog, AuthService) {
         var data = null;
         var token = '';
         var item;
@@ -18,7 +18,7 @@
 
         return service;
 
-        function getItem(){
+        function getItem() {
             return item;
         }
 
@@ -31,18 +31,18 @@
                 targetEvent: ev,
                 clickOutsideToClose: true
             })
-                .then(function (answer) {
-                    console.log(23, item)
-                    $http.post('http://sse.logicenergy.com/manager/sse/api/v1/appliance/' + item.appliance_id + '?access_token=' + vm.token, { "appliance": item }).then(function (ress) {
+                .then(function (item) {
+
+                    $http.post('http://sse.logicenergy.com/manager/sse/api/v1/appliance/' + item.appliance_id + '?access_token=' + AuthService.getToken(), { "appliance": item }).then(function (ress) {
                         console.log(ress);
                     });
                 }, function () {
                 });
         };
-        function DialogController($scope, $mdDialog,TempDialogService) {
+        function DialogController($scope, $mdDialog, TempDialogService) {
             var item = TempDialogService.getItem();
-            console.log(item,item.comfort_status[item.comfort_status.length - 1].profiles[item.comfort_status[item.comfort_status.length - 1].profiles.length - 1].set_point);
-            
+            console.log(item, item.comfort_status[item.comfort_status.length - 1].profiles[item.comfort_status[item.comfort_status.length - 1].profiles.length - 1].set_point);
+
             $scope.value = item.comfort_status[item.comfort_status.length - 1].profiles[item.comfort_status[item.comfort_status.length - 1].profiles.length - 1].set_point;
 
             $scope.hide = function () {
@@ -54,8 +54,11 @@
             };
 
             $scope.answer = function (answer) {
-                $mdDialog.hide(answer);
+                console.log($scope.value)
+                item.comfort_status[item.comfort_status.length - 1].profiles[item.comfort_status[item.comfort_status.length - 1].profiles.length - 1].set_point = $scope.value;
+                $mdDialog.hide(item);
             };
+       
         }
     }
 })();
